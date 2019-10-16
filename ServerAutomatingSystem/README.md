521238e4a1ddbb11341dde139e8f6e3d88553fbe
Kyle Chen <kyle@hacking-linux.com>
2019-10-16 17:20:16 +0800

    Change language, add initial command in README.md

--- ServerAutomatingSystem/README.md
+++ ServerAutomatingSystem/README.md
@@ -1,3 +1,8 @@
 # ServerAutomatingSystem
+## RUN 1st TIME
+-  Modify ServerAutomatingSystem/settings.py
+-  `python3 manage.py migrate` to initial tables.
+-  `python3 manage.py createsuperuser` to create super user.
+
 ## HOW TO RUN IT
 -  `python3 manage.py runserver 0.0.0.0:80`
--- ServerAutomatingSystem/ServerAutomatingSystem/settings.py
+++ ServerAutomatingSystem/ServerAutomatingSystem/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'MYSQL_SERVER_IP',
         'PORT': 'MYSQL_SERVER_PORT',
-        'NAME': 'mysql',
+        'NAME': 'MYSQL_DATABASE',
         'USER': 'MYSQL_USER',
         'PASSWORD': 'MYSQL_PASSWORD'
     }
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -115,7 +115,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
